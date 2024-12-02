c40a0288960245af09effa62a5a8459d7ca58263
Amin Gharibi <Gharibi8364@gmail.com>
2024-12-02 18:14:16 +0330

    [fix] update Custom Button component to show icons properly and fix the justify content to be center

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -16,10 +16,10 @@ CustomButton.propTypes = {
 
 function CustomButton({title, onClick, size, icon, leftIcon, rightIcon, isCurved, isSquared, isDashed, isOutline, isFilled}) {
     return (
-        <button onClick={onClick} style={{height: size + 'px'}} className={`flex items-center justify-between ${size === 32 ? 'px-2 gap-x-1' : size === 40 ? 'px-2 gap-x-2' : size === 48 || size === 56 ? 'px-4 gap-x-2' : ''} ${isOutline ? 'border border-primary sm:hover:bg-neutral2 text-primary' : isFilled ? 'bg-primary text-white' : ''} ${isDashed ? 'border-dashed' : ''} ${isCurved ? 'rounded-full' : isSquared ? 'rounded-lg' : ''} transition-colors`}>
-            {icon && leftIcon && <img src={icon} alt={title} className={`${size === 32 ? 'w-5 h-5' : size === 56 ? 'w-8 h-8' : 'w-6 h-6'}`}/>}
+        <button onClick={onClick} style={{height: size + 'px'}} className={`flex items-center justify-center ${size === 32 ? 'px-2 gap-x-1 text-sm' : size === 40 ? 'px-2 gap-x-2 text-sm' : size === 48 || size === 56 ? 'px-4 gap-x-2 sm:text-lg' : ''} ${isOutline ? 'border border-primary sm:hover:bg-neutral2 text-primary' : isFilled ? 'bg-primary text-white' : ''} ${isDashed ? 'border-dashed' : ''} ${isCurved ? 'rounded-full' : isSquared ? 'rounded-lg' : ''} transition-colors`}>
+            {icon && rightIcon && <img src={icon} alt={title} className={`${size === 32 ? 'w-5 h-5' : size === 56 ? 'w-8 h-8' : 'w-6 h-6'}`}/>}
             <span>{title}</span>
-            {icon && rightIcon && <img src={icon} alt={title} className={'w-6 h-6'}/>}
+            {icon && leftIcon && <img src={icon} alt={title} className={`${size === 32 ? 'w-5 h-5' : size === 56 ? 'w-8 h-8' : 'w-6 h-6'}`}/>}
         </button>
     );
 }
