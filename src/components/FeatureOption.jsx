60e7f6a3db0c8adf32c1ac6755f9f6e2ca320b2a
Amin Gharibi <Gharibi8364@gmail.com>
2024-12-02 20:21:30 +0330

    [fix] fix responsive form of Feature Option of landing page

--- src/components/FeatureOption.jsx
+++ src/components/FeatureOption.jsx
@@ -13,8 +13,8 @@ function FeatureOption({icon, title, description}) {
             <div className={'w-20 h-20 flex justify-center items-center rounded-full'} style={{backgroundColor: '#E5F2E9'}}>
                 <img src={icon} alt={title} className={'w-10 h-10'}/>
             </div>
-            <h4 className={'font-medium md:font-semibold md:text-xl text-neutral12 mt-2 text-center'}>{title}</h4>
-            <p className={'text-xs md:text-sm text-neutral10 text-center'}>{description}</p>
+            <h4 className={'font-medium sm:font-semibold sm:text-lg md:text-xl text-neutral12 mt-2 text-center'}>{title}</h4>
+            <p className={'text-xs sm:text-sm text-neutral10 text-center'}>{description}</p>
         </div>
     );
 }
