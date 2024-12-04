47314d02950de35c8cc98607f3a33a49ac315811
Amin Gharibi <Gharibi8364@gmail.com>
2024-12-04 21:48:38 +0330

    [fix] fix responsive bug in features section in home page

--- src/components/FeatureOption.jsx
+++ src/components/FeatureOption.jsx
@@ -10,8 +10,8 @@ FeatureOption.propTypes = {
 function FeatureOption({icon, title, description}) {
     return (
         <div className={'flex flex-col justify-center items-center'}>
-            <div className={'w-20 h-20 flex justify-center items-center rounded-full'} style={{backgroundColor: '#E5F2E9'}}>
-                <img src={icon} alt={title} className={'w-10 h-10'}/>
+            <div className={'w-[72px] h-[72px] sm:w-20 sm:h-20 flex justify-center items-center rounded-full'} style={{backgroundColor: '#E5F2E9'}}>
+                <img src={icon} alt={title} className={'w-8 h-8 sm:w-10 sm:h-10'}/>
             </div>
             <h4 className={'font-medium sm:font-semibold sm:text-lg md:text-xl text-neutral12 mt-2 text-center'}>{title}</h4>
             <p className={'text-xs sm:text-sm text-neutral10 text-center'}>{description}</p>
