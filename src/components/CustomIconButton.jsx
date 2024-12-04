89f3936b987bab552188de27a983f66e4d1591ff
Amin Gharibi <Gharibi8364@gmail.com>
2024-12-04 18:18:39 +0330

    [update] update CustomIconButton bg to white when outlined

--- src/components/CustomIconButton.jsx
+++ src/components/CustomIconButton.jsx
@@ -12,7 +12,7 @@ CustomIconButton.propTypes = {
 
 function CustomIconButton({onClick, size, icon, isCurved, isSquared, isOutline, isFilled}) {
     return (
-        <button onClick={onClick} style={{height: size + 'px', width: size + 'px'}} className={`flex items-center justify-center ${isOutline ? 'border border-primary sm:hover:bg-neutral2 text-primary' : isFilled ? 'bg-primary text-white' : ''} ${isCurved ? 'rounded-full' : isSquared ? 'rounded-lg' : ''} transition-colors`}>
+        <button onClick={onClick} style={{height: size + 'px', width: size + 'px'}} className={`flex items-center justify-center ${isOutline ? 'bg-white border border-primary sm:hover:bg-neutral2 text-primary' : isFilled ? 'bg-primary text-white' : ''} ${isCurved ? 'rounded-full' : isSquared ? 'rounded-lg' : ''} transition-colors`}>
             {icon && <img src={icon} className={`${size === 32 ? 'w-5 h-5' : size === 56 ? 'w-8 h-8' : 'w-6 h-6'}`}/>}
         </button>
     );
