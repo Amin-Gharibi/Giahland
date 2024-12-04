303ec1f4d70f10477aaabe30b6172f5c80d0a03c
Amin Gharibi <Gharibi8364@gmail.com>
2024-12-04 18:57:08 +0330

    [fix] fix cursor type in itemBox component

--- src/components/ItemBox.jsx
+++ src/components/ItemBox.jsx
@@ -12,8 +12,8 @@ function ItemBox({identifier, image, title, price}) {
     return (
         <div className={'flex flex-col sm:gap-6 border border-neutral5 rounded-xl p-4'}>
             <img className={'w-52 self-center sm:w-full h-64 object-contain max-sm:mt-4'} src={image} alt={title}/>
-            <span className={'text-base sm:text-lg leading-8 text-start text-black'}>{title}</span>
-            <div className={'flex items-center justify-between max-sm:mt-2 max-sm:mb-4'}>
+            <span className={'text-base sm:text-lg leading-8 text-start text-black cursor-default'}>{title}</span>
+            <div className={'flex items-center justify-between max-sm:mt-2 max-sm:mb-4 cursor-default'}>
                 <span className={'text-sm leading-6 text-black'}>قیمت:</span>
                 <span className={'text-base sm:text-lg leading-8 text-black'}>{price.toLocaleString('fa-IR')} تومان</span>
             </div>
