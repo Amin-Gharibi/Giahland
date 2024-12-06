f8bb1a92975f18259d9df55137f472f78059d1df
Amin Gharibi <Gharibi8364@gmail.com>
2024-12-06 23:24:42 +0330

    [chore] make SideMenuItem full responsive

--- src/components/SideMenuItem.jsx
+++ src/components/SideMenuItem.jsx
@@ -11,7 +11,7 @@ function SideMenuItem({Icon, title, href}) {
     return (
         <NavLink to={href} className={({isActive}) => isActive ? 'flex items-center gap-3 group active py-3 px-4 bg-[#F3FDFA] rounded-lg' : 'flex items-center gap-3 group py-3 px-4 bg-transparent hover:bg-neutral2 rounded-lg transition-colors'}>
             <Icon className={'w-6 h-6 fill-neutral10 group-[.active]:fill-primary'}/>
-            <span className={'text-lg leading-8 text-neutral10 group-[.active]:text-primary'}>{title}</span>
+            <span className={'lg:text-lg leading-8 text-neutral10 group-[.active]:text-primary'}>{title}</span>
         </NavLink>
     );
 }
