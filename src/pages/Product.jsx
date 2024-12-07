d973c39a91059791077d8195aeff182255832acd
Amin Gharibi <Gharibi8364@gmail.com>
2024-12-07 23:10:13 +0330

    [feat] create the image slider for product page and use it

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,13 @@ import {useEffect, useState} from 'react';
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import BreadCrumb from "../components/Breadcrumb.jsx";
+import ProductImagesSlider from "../components/ProductImagesSlider.jsx";
+import tempImage1 from "../assets/images/fourthBgImage.png";
+import tempImage2 from "../assets/images/firstBgImage.png";
+import tempImage3 from "../assets/images/secondBgImage.png";
+import tempImage4 from "../assets/images/thirdBgImage.png";
+import tempImage5 from "../assets/images/extraBgImage.png";
+import tempImage6 from "../assets/images/loginBgImage.png";
 
 function Product() {
     const productId = window.location.pathname.split('/')[2]
@@ -22,6 +29,13 @@ function Product() {
                 <Header/>
                 <div className={'mt-2 sm:mt-4 md:mt-6'}>
                     <BreadCrumb breadcrumbs={breadcrumbs ?? []}/>
+                    <div className={'sm:mt-10 md:mt-16 lg:mt-[90px] flex items-start'}>
+                        <div className={'w-full lg:w-[450px] h-[500px]'}>
+                            <ProductImagesSlider images={[
+                                tempImage1, tempImage2, tempImage3, tempImage4, tempImage5, tempImage6
+                            ]}/>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer/>
