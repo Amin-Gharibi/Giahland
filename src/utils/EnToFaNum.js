445896b8f0ad2977ea91e28a010e1cdc8ff06bc9
Amin Gharibi <Gharibi8364@gmail.com>
2024-12-08 13:46:13 +0330

    [chore] add grouping option to EnToFaNum function

--- src/utils/EnToFaNum.js
+++ src/utils/EnToFaNum.js
@@ -1,6 +1,14 @@
-const EnToFaNum = (number) => {
-    const persianDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹']
-    return number.toString().replace(/\d/g, (digit) => persianDigits[digit])
+const EnToFaNum = (number, useGrouping = false) => {
+    const persianDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+    let formattedNumber;
+    if (useGrouping) {
+        formattedNumber = new Intl.NumberFormat('en-US', {useGrouping: true}).format(number);
+    } else {
+        formattedNumber = number.toString();
+    }
+
+    return formattedNumber.replace(/\d/g, (digit) => persianDigits[digit]);
 }
 
 export default EnToFaNum;
