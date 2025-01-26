87ec5977826f10669b581904191a5921073482f7
Amin Gharibi <Gharibi8364@gmail.com>
2025-01-26 16:59:57 +0330

    [chore] configure api services

---
+++ src/config/api.config.js
@@ -0,0 +1,9 @@
+const API_CONFIG = {
+	BASE_URL: import.meta.env.API_BASE_URL || "http://localhost:8000/api",
+	TIMEOUT: 30000,
+	HEADERS: {
+		"Content-Type": "application/json",
+	},
+};
+
+export default API_CONFIG;
