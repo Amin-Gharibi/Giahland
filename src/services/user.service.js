83257c9c7fd61afd0e4af87282c09d78d6968555
Amin Gharibi <Gharibi8364@gmail.com>
2025-01-26 18:36:07 +0330

    [feat] add user services

---
+++ src/services/user.service.js
@@ -0,0 +1,68 @@
+import apiClient from "./api.service";
+
+export const UserService = {
+	async getMe() {
+		try {
+			const response = await apiClient.get("/users/me");
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	},
+	async updateProfile(firstName, lastName, phoneNumber, homeAddress, homePhoneNumber) {
+		try {
+			const response = await apiClient.put("/users/profile", { firstName, lastName, phoneNumber, homeAddress, homePhoneNumber });
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	},
+	async updatePassword(currentPassword, newPassword) {
+		try {
+			const response = await apiClient.put("/users/password", { currentPassword, newPassword });
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	},
+	async addAddress(address, ciy, province, postalCode) {
+		try {
+			const response = await apiClient.post("/users/addresses", { address, ciy, province, postalCode });
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	},
+	async getAddresses() {
+		try {
+			const response = await apiClient.get("/users/addresses");
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	},
+	async updateAddress(id, address, city, province, postalCode) {
+		try {
+			const response = await apiClient.put(`/users/addresses/${id}`, { address, city, province, postalCode });
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	},
+	async deleteAddress(id) {
+		try {
+			const response = await apiClient.delete(`/users/addresses/${id}`);
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	},
+	async setDefaultAddress(id) {
+		try {
+			const response = await apiClient.put(`/users/addresses/${id}/default`);
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	},
+};
