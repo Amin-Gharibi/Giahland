2b6add4da3e9536d3a47fcb21481700c4cf2c34b
Amin Gharibi <Gharibi8364@gmail.com>
2025-01-26 16:58:15 +0330

    [chore]: configure react-toastify

---
+++ src/config/toast.config.js
@@ -0,0 +1,41 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Default configuration for all toasts
+const defaultConfig = {
+	autoClose: 5000,
+	hideProgressBar: false,
+	closeOnClick: true,
+	pauseOnHover: true,
+	draggable: true,
+	progress: undefined,
+    className: "font-modam text-sm"
+};
+
+// Toast utility functions
+export const showToast = {
+	success: (message) => {
+		toast.success(message, {
+			...defaultConfig,
+			theme: "light",
+		});
+	},
+	error: (message) => {
+		toast.error(message, {
+			...defaultConfig,
+			theme: "light",
+		});
+	},
+	warning: (message) => {
+		toast.warning(message, {
+			...defaultConfig,
+			theme: "light",
+		});
+	},
+	info: (message) => {
+		toast.info(message, {
+			...defaultConfig,
+			theme: "light",
+		});
+	},
+};
